b992b3623950f411e1e30b54a049b25edb8a394d
whitewings-hub <54464375+whitewings-hub@users.noreply.github.com>
2022-06-25 21:22:40 +0800

    Update predict.py

--- code_2/predict.py
+++ code_2/predict.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""YoloV3 eval."""
+"""YoloV3 predict."""
 import os
 import argparse
 import datetime
